a27cd9506ed0ce9700258a22766d7080246484f6
Noah <z5292346@ad.unsw.edu.au>
2022-10-11 14:54:46 +1100

    Ui for player and easy ai implemented

--- src/dom/gameBoardDom.js
+++ src/dom/gameBoardDom.js
@@ -12,12 +12,13 @@ function createGameBoard(size, playID) {
       cell.setAttribute("data-status", 0);
       cell.setAttribute("data-x", row);
       cell.setAttribute("data-y", col);
+      cell.setAttribute("data-hit", 0);
       element.appendChild(cell);
     }
   }
   return element;
 }
-function populateGame(gameboard, playID) {
+function populateGame(gameboard, playID, visible) {
   const { grid } = gameboard;
   const size = grid[0].length;
   for (let row = 0; row < size; row += 1) {
@@ -30,6 +31,9 @@ function populateGame(gameboard, playID) {
         const ship = document.createElement("div");
         ship.classList.add(`ship`);
         ship.classList.add(`shipID-${status - 1}`);
+        if (!visible) {
+          ship.classList.add("invisible");
+        }
         cell.appendChild(ship);
         cell.setAttribute("data-status", status - 1);
       }
@@ -40,7 +44,7 @@ function updateGameboard(id) {
   const cell = document.getElementById(id);
   const hit = document.createElement("div");
   cell.classList.add("cellHit");
-
+  cell.setAttribute("data-hit", 1);
   hit.innerText = "❌";
   if (cell.dataset.status > 0) {
     hit.classList.add("hit");
@@ -51,4 +55,38 @@ function updateGameboard(id) {
     cell.appendChild(hit);
   }
 }
-export { createGameBoard, populateGame, updateGameboard };
+function displayGameOver(winner) {
+  return console.log(winner);
+}
+function removeInvisibility(playId, shipID) {
+  const gameboard = document.getElementById(`gameboard-playID-${playId}`);
+  const cells = gameboard.getElementsByClassName("grid-item");
+  for (let i = 0; i < cells.length; i += 1) {
+    const cell = cells[i];
+    const { status } = cell.dataset;
+    if (status == shipID) {
+      const ship = cell.getElementsByClassName("ship")[0];
+      if (ship && ship.classList.contains("invisible")) {
+        ship.classList.remove("invisible");
+      }
+    }
+  }
+}
+function createClickableGameboard(player, enemy, playID, func) {
+  const gameboard = document.getElementById(`gameboard-playID-${playID}`);
+  const cells = gameboard.getElementsByClassName("grid-item");
+  for (let i = 0; i < cells.length; i += 1) {
+    const cell = cells[i];
+    cell.addEventListener("click", () => {
+      func(player, enemy, playID, cell.dataset);
+    });
+  }
+}
+export {
+  createGameBoard,
+  populateGame,
+  updateGameboard,
+  createClickableGameboard,
+  removeInvisibility,
+  displayGameOver,
+};
--- src/factory/game.js
+++ src/factory/game.js
@@ -1,6 +1,10 @@
 import {
+  attackRandomGameBoard,
+  createClickableGameboard,
   createGameBoard,
+  displayGameOver,
   populateGame,
+  removeInvisibility,
   updateGameboard,
 } from "../dom/gameBoardDom";
 import Player from "./player";
@@ -29,13 +33,49 @@ const Game = () => {
     }
   };
   const isGameOver = (player1, player2) => {
-    if (player1.gameboard.shipsSunk) {
+    if (player1.gameboard.shipsSunk()) {
       return 1;
     }
-    if (player2.gameboard.shipsSunk) {
+    if (player2.gameboard.shipsSunk()) {
       return -1;
     }
     return 0;
+  };
+  const checkShips = (enemy, enemyID) => {
+    const enemyShips = enemy.gameboard.ships;
+    enemyShips.forEach((ship, i) => {
+      if (ship.isSunk()) {
+        removeInvisibility(enemyID, i + 1);
+        removeInvisibility(enemyID, 0);
+      }
+    });
+  };
+  const randomAttack = (player, enemy, playID) => {
+    const attack = player.randomAttack(enemy);
+    const attackID = `row-${attack.x}-col-${attack.y}-playID-${playID}`;
+    updateGameboard(attackID);
+  };
+  const easyAi = (player, enemy, playID) => {
+    if (isGameOver(player, enemy) == 0) {
+      randomAttack(player, enemy, playID);
+      checkShips(enemy, playID);
+    } else {
+      displayGameOver(isGameOver(enemy, player));
+    }
+  };
+  const playerTurn = (player, enemy, playID, data) => {
+    const { x, y, hit, status } = data;
+    if (isGameOver(player, enemy) == 0) {
+      if (parseInt(hit) === 0) {
+        player.attack(enemy, x, y);
+        const attackID = `row-${x}-col-${y}-playID-${playID}`;
+        updateGameboard(attackID);
+        checkShips(enemy, 1);
+        easyAi(enemy, player, 0);
+      }
+    } else {
+      displayGameOver(isGameOver(player, enemy));
+    }
   };
   const fakerGame = () => {
     const game = Game();
@@ -50,19 +90,16 @@ const Game = () => {
     game.randomShipsPlacments(player1);
     game.randomShipsPlacments(player2);
 
-    populateGame(player1.gameboard, 0);
-    populateGame(player2.gameboard, 1);
+    populateGame(player1.gameboard, 0, true);
+    populateGame(player2.gameboard, 1, false);
     for (let i = 0; i < 80; i += 1) {
       if (i % 2 === 0) {
-        const attack = player1.randomAttack(player2);
-        const attackID = `row-${attack.x}-col-${attack.y}-playID-1`;
-        updateGameboard(attackID);
+        easyAi(player1, player2, 1);
       } else {
-        const attack = player2.randomAttack(player1);
-        const attackID = `row-${attack.x}-col-${attack.y}-playID-0`;
-        updateGameboard(attackID);
+        easyAi(player2, player1, 0);
       }
     }
+    createClickableGameboard(player1, player2, 1, playerTurn);
   };
   return {
     placeShip,
